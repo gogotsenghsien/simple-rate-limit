f67b4b2174d9651e1cdbe193046098ebd4a2b3b2
Chris <gogotsenghsien@gmail.com>
2021-03-01 16:09:30 +0800

    Try compatible with Heroku

--- src/api/restful/server.go
+++ src/api/restful/server.go
@@ -1,14 +1,17 @@
 package restful
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gogotsenghsien/simple-rate-limit/src/api/restful/handlers"
+	"github.com/gogotsenghsien/simple-rate-limit/src/configs"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type Server struct {
+	config      *configs.Config
 	postHandler *handlers.PostHandler
 }
 
@@ -26,11 +29,13 @@ func (s *Server) Run() {
 	e.POST("/post", s.postHandler.AddPost)
 
 	// start server
-	e.Logger.Fatal(e.Start(":8080"))
+	port := s.config.GetInt("port")
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", port)))
 }
 
-func NewServer(postHandler *handlers.PostHandler) *Server {
+func NewServer(config *configs.Config, postHandler *handlers.PostHandler) *Server {
 	return &Server{
 		postHandler: postHandler,
+		config:      config,
 	}
 }
