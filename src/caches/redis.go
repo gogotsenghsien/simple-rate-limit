c61da6f82d5c08088a3e772fc238c9adf70fa82c
Chris <gogotsenghsien@gmail.com>
2021-03-01 15:49:44 +0800

    Try compatible with Heroku

--- src/caches/redis.go
+++ src/caches/redis.go
@@ -3,15 +3,17 @@ package caches
 import (
 	"github.com/go-redis/redis"
 	"github.com/gogotsenghsien/simple-rate-limit/src/configs"
+	"github.com/gogotsenghsien/simple-rate-limit/src/logs"
 )
 
 type Redis struct {
 	*redis.Client
 }
 
-func NewRedis(config *configs.Config) (*Redis, error) {
+func NewRedis(config *configs.Config, logger *logs.Logger) (*Redis, error) {
 	host := config.GetString("redis.url")
 	password := config.GetString("redis.password")
+	logger.Infof("Redis URI: %s", host)
 	client := redis.NewClient(&redis.Options{
 		Addr:     host,
 		Password: password,
