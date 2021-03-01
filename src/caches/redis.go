ce2aa94b6e133768b4c7b4dd5ec9da491a871576
Chris <gogotsenghsien@gmail.com>
2021-03-01 16:03:56 +0800

    Try compatible with Heroku

--- src/caches/redis.go
+++ src/caches/redis.go
@@ -1,6 +1,8 @@
 package caches
 
 import (
+	"strings"
+
 	"github.com/go-redis/redis"
 	"github.com/gogotsenghsien/simple-rate-limit/src/configs"
 	"github.com/gogotsenghsien/simple-rate-limit/src/logs"
@@ -11,9 +13,19 @@ type Redis struct {
 }
 
 func NewRedis(config *configs.Config, logger *logs.Logger) (*Redis, error) {
-	host := config.GetString("redis.url")
-	password := config.GetString("redis.password")
-	logger.Infof("Redis URI: %s", host)
+	uri := config.GetString("redis.url")
+	host, password := "", ""
+
+	// try compatible with Heroku redis
+	// e.g. redis://:pd3a2d4bd85868ce09020641447e3a4a15ca54fe9c4f83b40216ea27ae907fdd4@ec2-52-23-21-53.compute-1.amazonaws.com:26419
+	if strings.Contains(uri, "redis://") {
+		s := strings.Split(uri, "@")
+		host = s[1]
+		password = strings.Split(s[0], "redis://:")[1]
+	} else {
+		host = uri
+	}
+
 	client := redis.NewClient(&redis.Options{
 		Addr:     host,
 		Password: password,
